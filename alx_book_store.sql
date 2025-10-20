de1504f6c67a2ea7a468c828f9b5b75a27af665a
Nebiyu Yonas <165386126+Lordhokage7@users.noreply.github.com>
2025-10-20 14:12:05 +0300

    Modify database creation and email constraints

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
@@ -18,7 +18,7 @@ CREATE TABLE Books (
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE,
+    email VARCHAR(215),
     address TEXT
 );
 
@@ -37,10 +37,3 @@ CREATE TABLE Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
-
-
-
-
-
-
